901119583c54eeff33d45ddfcf7e2637364e9086
oharayu <kmm2459392@stu.o-hara.ac.jp>
2025-09-25 12:33:32 +0900

    Commit

--- lesson_sample/micro_site/controllers/addnumbers.py
+++ lesson_sample/micro_site/controllers/addnumbers.py
@@ -1,5 +1,5 @@
 from utils import parse_post, render_template
-
+from app_logic import  setFirstValue, setSecondValue
 def addnumbers(environ):
     method = environ["REQUEST_METHOD"]
 
@@ -7,6 +7,9 @@ def addnumbers(environ):
         data = parse_post(environ)
         num1 = data.get("num1", [""])[0]
         num2 = data.get("num2", [""])[0]
+
+        setFirstValue(num1)
+        setSecondValue(num2)
 
         try:
             n1 = int(num1)
